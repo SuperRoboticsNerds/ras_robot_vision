32027c549ec0dd4ef259616005423bc69da25d6c
adhaka <akashd@kth.se>
2015-11-17 15:58:11 +0100

    commiting main object detector file

--- ras_object_cv/ras_object_detect/src/object_detector_test.cpp
+++ ras_object_cv/ras_object_detect/src/object_detector_test.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
+#include <ras_object_lib/Image_Transfer.h>
+#include <ras_msgs/Object_id.h>
 
 // #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
@@ -14,6 +16,7 @@
 #include <ras_object_lib/image_utils.h>
 #include <ras_object_lib/image_color_range.h>
 #include <ras_object_lib/image_morph.h>
+#include <ras_object_lib/Image_Transfer.h>
 
 
 #include <stdio.h>
@@ -22,6 +25,8 @@
 #include <sstream>
 #include <vector>
 #include <cmath>
+#include <queue>
+#include <list>
 
 
 
@@ -57,22 +62,24 @@ int vl_def = 0;
 int vh_def = 255;
 int sl_def = 0;
 int sh_def = 255;
+int global_counter = 0;
 
 bool tranform_to_hsv = true;
 std::string morph_type = ras_cv::NO_MORPH;
 std::string color=ras_cv::GREEN_DARK;
 std::vector<std::string> object_colors;
 
+double sigma = 5.0;
 // std::string color=ras_cv::RED;
 // std::string blur_type = BLUR_NORMAL;
 int morph_size = 5;
-int blur_size = 5;
+int blur_size = 7;
 
 int mask_size_x = 3;
 int mask_size_y = 3;
 
-int min_area = 220;
-int max_area = 4800;
+int min_area = 500;
+int max_area = 8000;
 
 bool circular = false;
 bool filter_ratio = false;
@@ -83,6 +90,9 @@ int min_circularity = 15;
 // int min_inertia = 55;
 int min_convexity = 15;
 int min_inertia_ratio = 15;
+bool detect_all = true;
+int votes[7] = {0};
+std::list<int> votelist;
 
 
 int min_distance_between_blobs= 50;
@@ -90,6 +100,12 @@ cv::SimpleBlobDetector::Params params;
 cv::Mat thres_img;
 std::vector<cv::Mat> thres_imgs;
 std::vector<cv::KeyPoint> key_points;
+
+// client for the classifier service.
+ros::ServiceClient client_color;
+ros::ServiceClient client_shape;
+ros::ServiceClient client_material;
+ros::Publisher object_pub;
 
 
 static const int ROWS = 1;
@@ -105,6 +121,17 @@ static const int Y_OFF = 0;
 void colorTuneCallback(){
 
 }
+
+
+ras_object_lib::Image_Transfer  transferImage(const cv::Mat& cvimg){
+	sensor_msgs::ImagePtr rosimg;
+	ras_cv::to_ros_copy(cvimg, rosimg);
+	ras_object_lib::Image_Transfer imgtransfer;
+	imgtransfer.request.im = *rosimg;
+	return imgtransfer;
+}
+
+
 
 
 void  tuneCallback(const sensor_msgs::ImageConstPtr& inimg){
@@ -135,22 +162,29 @@ void  tuneCallback(const sensor_msgs::ImageConstPtr& inimg){
 	cv::Mat fin_img;
 
 	std::vector<cv::KeyPoint> key_points;
+	// std::list<std::string> votelist;
+	
 
 	ras_cv::to_cv_copy(&pr_img, inimg);
 
 	cv::cvtColor(pr_img, grayimg, CV_BGR2GRAY);
 
 
-	if(blur_type == BLUR_GAUSSIAN){
-		cv::GaussianBlur(pr_img, blurred_img, cv::Size(std::max(1, blur_size), std::max(1, blur_size)), 0, 0);
-	} else if(blur_type == BLUR_MEDIAN){
-		cv::medianBlur(pr_img, blurred_img, blur_size);
-	} else if(blur_type == BLUR_NORMAL){
-		cv::blur(pr_img, blurred_img, cv::Size(blur_size, blur_size), cv::Point(-1, -1));
-	} else{
-		blurred_img = pr_img;
-	}
+	// if(blur_type == BLUR_GAUSSIAN){
+	// 	cv::GaussianBlur(pr_img, blurred_img, cv::Size(std::max(1, blur_size), std::max(1, blur_size)), 0, 0);
+	// } else if(blur_type == BLUR_MEDIAN){
+	// 	cv::medianBlur(pr_img, blurred_img, blur_size);
+	// } else if(blur_type == BLUR_NORMAL){
+	// 	cv::blur(pr_img, blurred_img, cv::Size(blur_size, blur_size), cv::Point(-1, -1));
+	// } else{
+	// 	blurred_img = pr_img;
+	// }
 
+
+	ras_cv::Blur blur_params = 	ras_cv::Blur(blur_size, blur_size, sigma);
+
+
+	ras_cv::blur(pr_img, blurred_img, blur_type, blur_params);
 
 	ras_cv::morph_tranform(blurred_img, morphed_img, morph_type, cv::MORPH_RECT, morph_size);
 	// detector.detect(grayimg, key_points);
@@ -190,6 +224,11 @@ void  tuneCallback(const sensor_msgs::ImageConstPtr& inimg){
 	thres_img =  cv::Scalar(255, 255, 255) - thres_img;
 	fin_thres_img = cv::Scalar(255, 255, 255) - fin_thres_img;
 
+	if(detect_all == false){
+		fin_thres_img = thres_img;
+	}
+
+
 	cv::SimpleBlobDetector detector(params);
 	detector.detect(fin_thres_img, key_points);
 
@@ -199,23 +238,79 @@ void  tuneCallback(const sensor_msgs::ImageConstPtr& inimg){
 
 	// std::cout << "Number of detected key points:" << key_points.size() << "\n";
 
+	global_counter += 1;
 
 	if(key_points.size() >= 1){
-		std::cout <<
-		ras_cv::writeMatrixAsString<cv::Vec3b>(hsv_img(ras_cv::get_bounding_box(key_points[0], pr_img.size[1], pr_img.size[0], 0.55)))
-		<< std::endl;
+		// std::cout <<
+		// ras_cv::writeMatrixAsString<cv::Vec3b>(hsv_img(ras_cv::get_bounding_box(key_points[0], pr_img.size[1], pr_img.size[0], 0.55)))
+		// << std::endl;
 
 	}
 
-	for(int i =0; i < key_points.size(); i++){
+
+  	cv::imshow(WINDOW_NAME, fin_img);
+
+
+  	for(int i =0; i < key_points.size(); i++){
 
    		cv::imshow(
         POINT_WINDOW_NAME + " " + ras_cv::writeAsString(i), 
         pr_img(ras_cv::get_bounding_box(key_points[i], pr_img.cols, pr_img.rows)));
 
+        ras_object_lib::Image_Transfer  it = transferImage(pr_img(ras_cv::get_bounding_box(key_points[i], pr_img.cols, pr_img.rows, 0.60)));
+        ras_object_lib::Image_Transfer  it_shape = transferImage(pr_img(ras_cv::get_bounding_box(key_points[i], pr_img.cols, pr_img.rows, 1.50)));
+        // ras_object_lib::Image_Transfer  it_material = transferImage(pr_img(ras_cv::get_bounding_box(key_points[i], pr_img.cols, pr_img.rows, 1.50)));
+
+        std::vector<string> colors_in_frames;
+
+        if(client_color.call(it)){
+        	if(votelist.size() >= 10){
+        		for (std::list<int>::const_iterator it = votelist.begin(), end = votelist.end(); it != end; ++it){
+        			int index = *it;
+        			votes[index]++;
+        		}
+        		std::cout << "hiii";
+        		std:cout << votes;
+        		int col_index = ras_cv::argmax(votes, 7);
+        		int maxval = ras_cv::maxval(votes, 7);
+        		if(maxval >= 7){
+        			while(!votelist.empty()){
+        				votelist.pop_back();        				
+        			}
+        			for(int i =0; i< 7; i++){
+        				votes[i] =0;
+        			}
+        			// some color detected yay !!!!!!
+        			// publish here ..........
+        			ras_msgs::Object_id oid;
+        			oid.object = 1;
+        			oid.color = col_index;
+        			oid.shape = 1;
+        			oid.time = 1000;
+        			oid.x = 1;
+        			oid.y = 10;
+        			object_pub.publish(oid);
+        			std::cout << col_index << "\n";
+        			std::cout << "some color" << "\n";
+
+        		}
+
+        	}
+
+        	int number = atoi(it.response.str.c_str());
+        	// it.response.str << number;
+        	votelist.push_back(number);
+
+        	std::cout << it.response.str << "\n";
+        	std::cout << votelist.size() << "\n";
+        	string color = it.response.str;
+        	
+        // 	// client_color.call(it);
+        }
+
 	}
 
-  	cv::imshow(WINDOW_NAME, fin_img);
+	
   	// cv::imshow(WINDOW_NAME, thres_img);
   	cv::waitKey(1);
 
@@ -234,6 +329,7 @@ int main(int argc, char ** argv){
   	cv::namedWindow(TRACKBAR_WINDOW, CV_WINDOW_NORMAL);
   	cv::moveWindow(TRACKBAR_WINDOW, 1050, 250);
 
+
   	// fill the vector of colors
   	object_colors.push_back(ras_cv::GREEN_DARK);
   	object_colors.push_back(ras_cv::GREEN_FL);
@@ -249,6 +345,13 @@ int main(int argc, char ** argv){
 
 	ros::NodeHandle node("~");
 	ros::Subscriber sub = node.subscribe("/camera/rgb/image_raw", MAX_BUFFER, tuneCallback);
+
+  	client_color = node.serviceClient<ras_object_lib::Image_Transfer>("/classify_objects/color");
+  	client_shape = node.serviceClient<ras_object_lib::Image_Transfer>("/classify_objects/shape");
+  	client_material = node.serviceClient<ras_object_lib::Image_Transfer>("/classify_objects/material");
+
+  	object_pub = node.advertise<ras_msgs::Object_id>("/object_pub", 1);
+
 
 	ros::Rate loop_rate(LOOP_RATE);
 
